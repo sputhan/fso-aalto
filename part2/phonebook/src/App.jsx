653f696bbbc1905a2e8f26c83846c37966b7634e
Sheena Puthan <sheena.puthan@gmail.com>
2024-08-20 09:48:57 +0300

    ex 2.17 done

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,7 +3,8 @@ import axios from 'axios'
 import Person from './components/Person'
 import SearchField from './components/SearchField'
 import AddPersonForm from './components/AddPersonForm'
-import personService from  './services/persons'
+import Notification from './components/Notification'
+import personService from './services/persons'
 
 
 const App = (props) => {
@@ -11,6 +12,8 @@ const App = (props) => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterStr, setFilterStr] = useState('')
+  const [notification, setNotification] = useState(null)
+
 
   const hook = () => {
     console.log('effect')
@@ -21,7 +24,7 @@ const App = (props) => {
         setPersons(response.data)
       })
   }
-  
+
   useEffect(hook, [])
 
 
@@ -48,13 +51,21 @@ const App = (props) => {
     console.log(persons)
     if (persons.filter(person => person.name == newName).length > 0) {
       console.log("true");
-      if (window.confirm(`${newName} is already added to phonebook, replace phone number ?`)){
+      if (window.confirm(`${newName} is already added to phonebook, replace phone number ?`)) {
         let personObject = persons.filter(person => person.name == newName)[0]
-        personObject = {...personObject, number:newNumber}
+        personObject = { ...personObject, number: newNumber }
         personService.update(personObject.id, personObject)
-        .then(returnedPerson => {
-          setPersons(persons.map(p => p.id !== returnedPerson.id ? p : returnedPerson))
-        })
+          .then(returnedPerson => {
+            setPersons(persons.map(p => p.id !== returnedPerson.id ? p : returnedPerson))
+          }).catch(error => {
+            setNotification({
+              message: `Information of '${newName}' was already removed from server`, isSuccess: false
+            })
+            setTimeout(() => {
+              setNotification(null)
+            }, 5000)
+            setNotes(persons.filter(n => n.id !== id))
+          })
 
       }
     }
@@ -62,20 +73,25 @@ const App = (props) => {
       console.log("false");
       const personObject = { name: newName, number: newNumber }
       personService.create(personObject)
-      .then(returnedPerson => {
-        setPersons(persons.concat(returnedPerson))
-      })
-      
+        .then(returnedPerson => {
+
+          setNotification({ message: `Added ${returnedPerson.name}`, isSuccess: true })
+          setTimeout(() => {
+            setNotification(null)
+          }, 5000)
+          setPersons(persons.concat(returnedPerson))
+        })
+
     }
   }
 
   const handleDelete = (person) => {
     console.log("delete person")
-    if (window.confirm(`Delete ${person.name}`)){
+    if (window.confirm(`Delete ${person.name}`)) {
       personService.remove(person.id)
-      .then(deletedPerson => {
-        setPersons(persons.filter(p => p.id !== deletedPerson.id))
-      })
+        .then(deletedPerson => {
+          setPersons(persons.filter(p => p.id !== deletedPerson.id))
+        })
 
     }
 
@@ -85,13 +101,14 @@ const App = (props) => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification notification={notification} />
       <SearchField keyword={filterStr} handler={handleFilter} />
 
       <h2>Add New</h2>
       <AddPersonForm name={newName} nameHandler={handleNameChange} number={newNumber} numberHandler={handleNumberChange} submitHandler={handleAddName} />
       <h2>Numbers</h2>
       {
-        personsToShow.map(person => <Person key={person.name} person={person}  deleteHandler={() => handleDelete(person)}/>)
+        personsToShow.map(person => <Person key={person.name} person={person} deleteHandler={() => handleDelete(person)} />)
       }
 
     </div>
